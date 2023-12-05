717ab502e7fb1e5683e751d6d1d5a02b2904e60d
riefive <muhammad.rifai@salt.co.id>
2023-12-05 13:06:36 +0700

    feat(ui): add header in editorjs

---
+++ app/(samples)/editor/page.tsx
@@ -0,0 +1,9 @@
+import { EditorComponent } from "@/components/pages/EditorComponent";
+
+export default function EditorPage() {
+    return (
+        <div className="App my-2">
+            <EditorComponent />
+        </div>
+    );
+}
